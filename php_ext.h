9705bceb50663299b8523efc8e6dc11c8d5e93c0
ZhangBaoCai <talent518@yeah.net>
2020-11-27 00:35:35 +0800

    添加ssp_var_inc函数。

--- php_ext.h
+++ php_ext.h
@@ -241,6 +241,7 @@ static PHP_FUNCTION(ssp_var_init);
 static PHP_FUNCTION(ssp_var_exists);
 static PHP_FUNCTION(ssp_var_get);
 static PHP_FUNCTION(ssp_var_put);
+static PHP_FUNCTION(ssp_var_inc);
 static PHP_FUNCTION(ssp_var_del);
 static PHP_FUNCTION(ssp_var_clean);
 static PHP_FUNCTION(ssp_var_destory);
